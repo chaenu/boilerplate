8797d1b5f3130c7aeab0a19afbd5bae9d8b8b783
Känel, Thomas <Thomas.Kaenel@aperto.com>
2017-08-29 15:27:52 +0200

    Inital commit

---
+++ src/components/my-component/my-component.js
@@ -0,0 +1,29 @@
+/**
+ * MyComponent
+ * 
+ * Description. 
+ *
+ * @version v1.0.0
+ * 
+ * @author Thomas Kaenel
+ */
+
+export default class MyComponent {
+	constructor(){
+		this.init();
+	}
+
+	/**
+	 * Initialize the component
+	 */
+	init() {
+		this.render();
+	}
+
+	/**
+	 * Render to DOM. 
+	 */
+	render() {
+		// render
+	}	
+}
---
+++ src/js/main.js
@@ -0,0 +1,7 @@
+// Import components
+// import MyComponent from '../components/my-component/my-component';
+
+// Initialize components
+// new MyComponent();
+
+console.log('App successfully initialized');
